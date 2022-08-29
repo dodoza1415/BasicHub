6a8606685a57c41d952569c5e644b4d1af906301
Surachet <mcmonkung@gmail.com>
2022-08-29 19:08:32 +0700

    Update new file

---
+++ main.js
@@ -0,0 +1,4 @@
+// alert("Hello World");
+//console.log("Hello world");
+document.getElementById("main").innerHTML = 'Hello World';
+//document.write("Hello World") เป็น DOM เหมือนอันบน แต่ไม่นิยมใช้เพราะจะเป็นการเขียนทับข้อมูลทั้งหมดไปเลย
